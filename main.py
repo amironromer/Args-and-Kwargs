1b34ac1ac827fce2fe0467f4987c36c318c84bf5
Alejandro Miron <113551832+amironromer@users.noreply.github.com>
2022-11-10 20:13:47 +0000

    alot

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def my_sum(*args):
-  print(args)
+# def my_sum(*args):
+#   print(args)
 
-print(my_sum(3,4,5))
-
-
+# print(my_sum(3,4,5))
 
 
 
@@ -13,14 +11,45 @@ print(my_sum(3,4,5))
 
 
 
-# args for entering as many arguments possible
+
+
+# # args for entering as many arguments possible
+# def a_sum(*args):
+#   for num in args:
+#     print(num)
+
+# print(a_sum(1))
+# print(a_sum(1,3))
+# print(a_sum(1,3,4,5,6))
+
+# list=[3,4,5,6,7,8,9,45,67]
+# def sum_num(num1,num2,num3):
+#   sum=0
+#   for num in list:
+#     sum=sum+num
+#   return sum
+# print(sum(list))
+
 def a_sum(*args):
-  for num in args:
-    print(num)
+  # total=0
+  # for num in args:
+  #   total += num
+  # return total
+  return sum(args)
+print(a_sum(4,56,77,88,9,45,76))
 
-print(a_sum(1))
-print(a_sum(1,3))
-print(a_sum(1,3,4,5,6))
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
